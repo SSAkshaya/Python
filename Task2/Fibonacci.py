10d91d41157ab182ba88bcd571d14387a83e564e
SSAkshaya <67408261+SSAkshaya@users.noreply.github.com>
2020-09-09 21:50:07 +0530

    Fibonacci series
    To print Fibonacci series less than 1 lakh.

---
+++ Task2/Fibonacci.py
@@ -0,0 +1,13 @@
+i = 0
+j = 1
+print('\n')
+print("PRINTING FIBONACCI LESS THAN 1 LAKH")
+print('\n')
+print(i , end=", ")
+print(j , end=", ")
+while j<100000:
+     i=i+j
+     j=j+i
+     print(i , end=", ")
+     print(j , end=", ")
+print('\n')
