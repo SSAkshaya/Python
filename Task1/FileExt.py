78830e7e86dd3bc4cd8952a5ef2a4e838599a6d0
SSAkshaya <67408261+SSAkshaya@users.noreply.github.com>
2020-09-11 16:18:23 +0530

    FileExt.py
    To get the file extension for python, java and C++ files as output.

--- Task1/FileExt.py
+++ Task1/FileExt.py
@@ -1,3 +1,8 @@
 filename = input("Input the Filename: ")
 f_extns = filename.split(".")
-print(f_extns[-1])
+if f_extns[-1]=="py":
+	print("The extension of the file is: 'python'")
+elif f_extns[-1]=="java":
+	 print("The extension of the file is: 'Java'")
+elif f_extns[-1]=="cpp":
+      print("The extension of the file is: 'C++'")
